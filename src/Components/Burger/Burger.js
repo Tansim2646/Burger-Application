e919b843c84bdf355f99a36d9e92f5dfd8b82736
Tansim Anjum <tansimanim2646@gmail.com>
2023-06-09 18:25:36 +0600

    burger resposiveness added

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -4,11 +4,11 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient.js'
 const Burger = ()=>{
     return(
         <div className={classes.Burger}>
-            <BurgerIngredient type='burger-top' />
-            <BurgerIngredient type='cheese' />
-            <BurgerIngredient type='salad' />
-            <BurgerIngredient type='meat' />
-            <BurgerIngredient type='burger-bottom' />
+            <BurgerIngredient type='burger-top'/>
+            <BurgerIngredient type='cheese'/>
+            <BurgerIngredient type='salad'/>
+            <BurgerIngredient type='meat'/>
+            <BurgerIngredient type='burger-bottom'/>
         </div>
     );
 }
